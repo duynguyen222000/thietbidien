3495b6f04adeeefd782e7751296e32267b5fbb07
DuyND <nguyenducduy02022000@gmail.com>
2024-12-17 14:25:57 +0700

    footer init

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-top">
+        <div className="container">
+          <p className="footer-top-text"></p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
